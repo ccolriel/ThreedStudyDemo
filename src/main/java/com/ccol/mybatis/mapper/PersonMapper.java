d37eeb64c07f0c46526311f9d79834a3e8b8950c
wangpengcheng <wangpengcheng@mail01.huawei.com>
2019-01-25 11:43:52 +0800

    使用注解方式 不用Mpper.xml执行sql

--- src/main/java/com/ccol/mybatis/mapper/PersonMapper.java
+++ src/main/java/com/ccol/mybatis/mapper/PersonMapper.java
@@ -1,7 +1,10 @@
 package com.ccol.mybatis.mapper;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.ccol.mybatis.bean.Person;
 
 public interface PersonMapper {
+	//@Select("select * from person where pid = #{id}")  使用注解方式 不用Mpper.xml执行sql
 	Person selectPerson(int pid);
 }
